d5d67971e1411785759da80ae4726123c59f562a
Ilya Zhuravlev <whatever@xyz.is>
2020-12-06 19:23:57 -0500

    vial: initial encoder support

--- quantum/dynamic_keymap.c
+++ quantum/dynamic_keymap.c
@@ -68,9 +68,20 @@
 #    define DYNAMIC_KEYMAP_EEPROM_ADDR DYNAMIC_KEYMAP_EEPROM_START
 #endif
 
-// Dynamic encoders starts after dynamic keymaps
+// Encoders are located right after the dynamic keymap
+#define VIAL_ENCODERS_EEPROM_ADDR (DYNAMIC_KEYMAP_EEPROM_ADDR + (DYNAMIC_KEYMAP_LAYER_COUNT * MATRIX_ROWS * MATRIX_COLS * 2))
+
+#ifdef VIAL_ENCODERS_ENABLE
+#define NUMBER_OF_ENCODERS (sizeof(encoders_pad_a) / sizeof(pin_t))
+static pin_t encoders_pad_a[] = ENCODERS_PAD_A;
+#define VIAL_ENCODERS_SIZE (NUMBER_OF_ENCODERS * DYNAMIC_KEYMAP_LAYER_COUNT * 2 * 2)
+#else
+#define VIAL_ENCODERS_SIZE 0
+#endif
+
+// Dynamic encoders starts after encoders, or dynamic keymaps if encoders aren't enabled
 #ifndef DYNAMIC_KEYMAP_ENCODER_EEPROM_ADDR
-#    define DYNAMIC_KEYMAP_ENCODER_EEPROM_ADDR (DYNAMIC_KEYMAP_EEPROM_ADDR + (DYNAMIC_KEYMAP_LAYER_COUNT * MATRIX_ROWS * MATRIX_COLS * 2))
+#    define DYNAMIC_KEYMAP_ENCODER_EEPROM_ADDR (VIAL_ENCODERS_EEPROM_ADDR + VIAL_ENCODERS_SIZE)
 #endif
 
 // Dynamic macro starts after dynamic encoders, but only when using ENCODER_MAP
@@ -150,6 +161,31 @@ void dynamic_keymap_set_encoder(uint8_t layer, uint8_t encoder_id, bool clockwis
 }
 #endif // ENCODER_MAP_ENABLE
 
+#ifdef VIAL_ENCODERS_ENABLE
+static void *dynamic_keymap_encoder_to_eeprom_address(uint8_t layer, uint8_t idx, uint8_t dir) {
+    return ((void *)VIAL_ENCODERS_EEPROM_ADDR) + (layer * NUMBER_OF_ENCODERS * 2 * 2) + (idx * 2 * 2) + dir * 2;
+}
+
+uint16_t dynamic_keymap_get_encoder(uint8_t layer, uint8_t idx, uint8_t dir) {
+    if (layer >= DYNAMIC_KEYMAP_LAYER_COUNT || idx >= NUMBER_OF_ENCODERS || dir > 1)
+        return 0;
+
+    void *address = dynamic_keymap_encoder_to_eeprom_address(layer, idx, dir);
+    uint16_t keycode = eeprom_read_byte(address) << 8;
+    keycode |= eeprom_read_byte(address + 1);
+    return keycode;
+}
+
+void dynamic_keymap_set_encoder(uint8_t layer, uint8_t idx, uint8_t dir, uint16_t keycode) {
+    if (layer >= DYNAMIC_KEYMAP_LAYER_COUNT || idx >= NUMBER_OF_ENCODERS || dir > 1)
+        return;
+
+    void *address = dynamic_keymap_encoder_to_eeprom_address(layer, idx, dir);
+    eeprom_update_byte(address, (uint8_t)(keycode >> 8));
+    eeprom_update_byte(address + 1, (uint8_t)(keycode & 0xFF));
+}
+#endif
+
 void dynamic_keymap_reset(void) {
     // Reset the keymaps in EEPROM to what is in flash.
     for (int layer = 0; layer < DYNAMIC_KEYMAP_LAYER_COUNT; layer++) {
@@ -164,6 +200,13 @@ void dynamic_keymap_reset(void) {
             dynamic_keymap_set_encoder(layer, encoder, false, keycode_at_encodermap_location_raw(layer, encoder, false));
         }
 #endif // ENCODER_MAP_ENABLE
+
+#ifdef VIAL_ENCODERS_ENABLE
+    for (int idx = 0; idx < NUMBER_OF_ENCODERS; ++idx) {
+        dynamic_keymap_set_encoder(layer, idx, 0, KC_TRNS);
+        dynamic_keymap_set_encoder(layer, idx, 1, KC_TRNS);
+    }
+#endif
     }
 }
 
